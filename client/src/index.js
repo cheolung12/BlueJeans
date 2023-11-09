a6141e123d0242971fcc08fd7c46a20e07281879
cheolung12 <109952479+cheolung12@users.noreply.github.com>
2023-11-10 03:22:16 +0900

    [feat] router

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import NotFound from './pages/NotFound';
+import Main from './pages/Main';
+import Recruitment from './pages/Recruitment';
+import EBook from './pages/EBook';
+import Chat from './pages/Chat';
+import Essay from './pages/Essay';
+import FindHome from './pages/FindHome';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, element: <Main /> },
+      { path: 'recuritment', element: <Recruitment /> },
+      { path: 'ebook', element: <EBook /> },
+      { path: 'chat', element: <Chat /> },
+      { path: 'essay', element: <Essay /> },
+      { path: 'findhome', element: <FindHome /> },
+    ],
+  },
+]);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(
+  <>
+    <RouterProvider router={router} />
+  </>
+);
