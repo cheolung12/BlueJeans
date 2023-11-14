2740ce8ecb9d2473b6100d194f577f71eceaad9e
LeeYoungKyung <yklee1040@naver.com>
2023-11-14 15:58:15 +0900

    [feat] house address

--- client/src/pages/FindHome.jsx
+++ client/src/pages/FindHome.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Map from '../components/home/Map';
+import AddressButton from '../components/home/AddressButton';
 
 export default function FindHome() {
   return (
     <div>
+      <AddressButton></AddressButton>
       <Map></Map>
     </div>
   );
