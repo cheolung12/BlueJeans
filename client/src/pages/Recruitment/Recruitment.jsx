d100c60eb5d2b28c8617c3b69bf595227d0c3527
NavyBlueSky <lylebang9397@gmail.com>
2023-11-15 17:05:10 +0900

    [feat] 페이지네이션

--- client/src/pages/Recruitment/Recruitment.jsx
+++ client/src/pages/Recruitment/Recruitment.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PageButtonSection from '../../components/Recruitment/Main/PageButtonSection';
 import SelectRecruit from '../../components/Recruitment/Main/SelectRecruit';
 import CardSection from '../../components/Recruitment/Main/CardSection';
+import Paging from '../../components/Recruitment/Main/Paging';
 
 export default function Recruitment() {
     return (
@@ -9,12 +10,16 @@ export default function Recruitment() {
             <nav>
                 <SelectRecruit />
             </nav>
+            {/*
+
             <section>
                 <CardSection />
             </section>
             <nav>
                 <PageButtonSection />
             </nav>
+             */}
+            <Paging />
         </>
     );
 }
