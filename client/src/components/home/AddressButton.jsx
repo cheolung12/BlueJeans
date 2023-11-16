6195dbb9ddc4e73564f92244b3b51816b24777e4
LeeYoungKyung <yklee1040@naver.com>
2023-11-17 02:34:28 +0900

    [fix] marker

--- client/src/components/home/AddressButton.jsx
+++ client/src/components/home/AddressButton.jsx
@@ -3,7 +3,7 @@ import { FcHome } from 'react-icons/fc';
 
 export default function AddressButton({ setUserAddress }) {
   const handleClick = () => {
-    setUserAddress('서울특별시 송파구 법원로 55');
+    setUserAddress('서울특별시 송파구 송파대로 111');
   };
 
   return (
