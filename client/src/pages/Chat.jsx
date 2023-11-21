ee1790ad2baf7585f8c2b933ac3ca069202d802f
Asong Ma <136426795+asonghub@users.noreply.github.com>
2023-11-21 16:04:10 +0900

    [feat] api 주소 변경

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,11 +1,11 @@
-import React, { useState, useRef } from 'react';
-import axios from 'axios';
-import ChatBox from '../components/chat/ChatBox';
-import ChatInput from '../components/chat/ChatInput';
-import ChatButton from '../components/chat/ChatButton';
+import React, { useState, useRef } from "react";
+import axios from "axios";
+import ChatBox from "../components/chat/ChatBox";
+import ChatInput from "../components/chat/ChatInput";
+import ChatButton from "../components/chat/ChatButton";
 
 const ChatApp = () => {
-  const [inputText, setInputText] = useState('');
+  const [inputText, setInputText] = useState("");
   const [chatMessages, setChatMessages] = useState([]);
   const [isLoading, setIsLoading] = useState(false); // 말줄임표 표시용
   const inputRef = useRef(null); // input focus를 위한 ref
@@ -13,11 +13,11 @@ const ChatApp = () => {
 
   const handleSendClick = async () => {
     // 빈 값 예외처리
-    if (inputText.trim() === '') {
+    if (inputText.trim() === "") {
       inputRef.current.focus();
-      inputRef.current.style.outline = '2px solid red'
+      inputRef.current.style.outline = "2px solid red";
       setTimeout(() => {
-        inputRef.current.style.outline = '';
+        inputRef.current.style.outline = "";
       }, 2000);
       return;
     }
@@ -28,7 +28,7 @@ const ChatApp = () => {
       { text: inputText, isMine: true },
     ];
     setChatMessages(newChatMessages);
-    setInputText('');
+    setInputText("");
 
     // 이전 대화 저장
     const previousConversation =
@@ -39,8 +39,8 @@ const ChatApp = () => {
     // 챗봇에게 응답 받아와서 추가
     try {
       const response = await axios({
-        method: 'POST',
-        url: 'http://localhost:8080/chat-gpt/question',
+        method: "POST",
+        url: "http://localhost:8080/api/chat/question",
         data: {
           question: inputText,
           previousConversation,
@@ -48,15 +48,15 @@ const ChatApp = () => {
       });
 
       const responseText = response.data.choices[0].text
-        .replace(/\n/g, '')
-        .replace('A: ', ''); // 형식 맞추기
+        .replace(/\n/g, "")
+        .replace("A: ", ""); // 형식 맞추기
       const updatedChatMessages = [
         ...newChatMessages,
         { text: responseText, isMine: false },
       ];
       setChatMessages(updatedChatMessages);
     } catch (error) {
-      console.log('전송 오류: ', error);
+      console.log("전송 오류: ", error);
     } finally {
       setIsLoading(false);
       console.log(previousConversation);
@@ -64,16 +64,16 @@ const ChatApp = () => {
   };
 
   return (
-    <div className='flex justify-center fade-in mb-28'>
+    <div className="flex justify-center fade-in mb-28">
       {/* content wrapper */}
-      <div className='w-3/5'>
+      <div className="w-3/5">
         <ChatBox
           inputText={inputText}
           chatMessages={chatMessages}
           isLoading={isLoading}
         />
         {/* input wrapper */}
-        <div className='flex justify-end'>
+        <div className="flex justify-end">
           <ChatInput
             inputText={inputText}
             setInputText={setInputText}
