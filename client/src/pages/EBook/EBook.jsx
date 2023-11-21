43c9fba662f2cb3971cbfcfeb3c275dda69cd7fd
유민 <gkjm123@naver.com>
2023-11-21 14:33:01 +0900

    [design] ebook 메인 수정

--- client/src/pages/EBook/EBook.jsx
+++ client/src/pages/EBook/EBook.jsx
@@ -5,6 +5,7 @@ import Title from '../../components/Ebook/Main/Title';
 import { useParams, Link } from 'react-router-dom';
 import bookList from '../../data/bookList.json';
 import BookCard from '../../components/Ebook/Main/BookCard';
+import BookCardSearch from '../../components/Ebook/Main/BookCardSearch';
 
 const books = bookList.books;
 
@@ -19,48 +20,41 @@ export default function EBook() {
       )
     : books;
 
-  // 검색어가 있는 경우 레이아웃 변경
-  const containerStyle = searchInput
-    ? { display: 'block' }
-    : { display: 'flex' };
-
   return (
     <section className='px-[11rem]'>
       <div>
         <Title />
-        <div className='flex items-center justify-between '>
+        <div className='flex items-center justify-between px-8'>
           <Filter />
           <SearchBooks book={books} />
         </div>
 
-        <div
-          style={containerStyle}
-          className='flex flex-wrap bg-gray-300 justify-center'
-        >
-          {filterBooks.length > 0 ? (
-            <>
-              {searchInput && <p>{`'${searchInput}'의 검색결과`}</p>}
-              {filterBooks.map((book) => (
-                <Link
-                  to={`/ebook/detail/${book.id}`}
-                  className='p-2'
-                  key={book.id}
-                >
+        <div className='flex flex-wrap justify-center'>
+          <>
+            {filterBooks.map((book) => (
+              <Link
+                to={`/ebook/detail/${book.id}`}
+                className='p-2'
+                key={book.id}
+              >
+                {searchInput ? (
+                  <BookCardSearch
+                    id={book.id}
+                    thumbnail={book.thumbnail}
+                    title={book.title}
+                    author={book.author}
+                  />
+                ) : (
                   <BookCard
                     id={book.id}
                     thumbnail={book.thumbnail}
                     title={book.title}
                     author={book.author}
                   />
-                </Link>
-              ))}
-            </>
-          ) : (
-            <div>
-              <div>{`'${searchInput}'의 검색결과`}</div>
-              <p>검색결과가 없습니다.</p>
-            </div>
-          )}
+                )}
+              </Link>
+            ))}
+          </>
         </div>
       </div>
     </section>
