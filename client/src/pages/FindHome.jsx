0d7f7a19fb038d7300f8341dae5cecc68c4c0d7f
LeeYoungKyung <128415146+LeeYoungKyung@users.noreply.github.com>
2023-11-22 20:46:09 +0900

    [fix] navbar

--- client/src/pages/FindHome.jsx
+++ client/src/pages/FindHome.jsx
@@ -1,14 +1,12 @@
 import React, { useState } from 'react';
 import Map from '../components/home/Map';
 import AddressButton from '../components/home/AddressButton';
-import Navbar from '../pages/Navbar';
 
 export default function FindHome() {
   const [userAddress, setUserAddress] = useState('');
 
   return (
     <div>
-      <Navbar></Navbar>
       <AddressButton setUserAddress={setUserAddress}></AddressButton>
       <Map userAddress={userAddress}></Map>
     </div>
