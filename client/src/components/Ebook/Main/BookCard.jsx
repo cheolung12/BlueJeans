5ab707d12d061d0f1422dd406f3c925fadbc169b
유민 <gkjm123@naver.com>
2023-11-22 14:45:01 +0900

    [refactor] ebook 리팩토링, css

--- client/src/components/Ebook/Main/BookCard.jsx
+++ client/src/components/Ebook/Main/BookCard.jsx
@@ -6,13 +6,13 @@ export default function BookCard({ id, thumbnail, title, author }) {
     <div>
       <div
         key={id}
-        className=' flex flex-col items-center justify-around bg-gray-400 w-[11rem] h-[22rem] rounded-[0.5rem] bg-gray-200'
+        className=' flex flex-col items-center justify-around w-[11rem] h-[22rem] rounded-[0.5rem] bg-gray-200'
       >
         <div>
           <img src={thumbnail} alt='book' className=' w-[7rem] h-40' />
         </div>
         <div className='flex flex-col text-center w-[10rem] h-[8rem] justify-between text-black'>
-          <div className='h-[4rem] font-semibold text-xl'>{title}</div>
+          <div className='h-[4rem] font-semibold text-lg'>{title}</div>
           <div>{author}</div>
           {/* <div className='flex flex-col'>
             <div className='flex justify-center mt-5'>
