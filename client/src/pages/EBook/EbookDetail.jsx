2deafe0b71720de3711b337b016a17ffc3a05642
유민 <gkjm123@naver.com>
2023-11-22 14:42:50 +0900

    [refactor] 라우터 변경

--- client/src/pages/EBook/EbookDetail.jsx
+++ client/src/pages/EBook/EbookDetail.jsx
@@ -1,33 +1,18 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
-import bookList from '../../data/bookList.json';
+import { useLocation } from 'react-router-dom';
 import Title from '../../components/Ebook/Main/Title';
-import ResButton from '../../components/common/ResButton';
-import { Link } from 'react-router-dom';
 import BookCardDetail from '../../components/Ebook/Detail/BookCardDetail';
 
-const books = bookList.books;
-
 export default function EBookDetail() {
-  const { bookId } = useParams();
-  // map 대신 find 사용
-  const findBook = books.find((book) => book.id === bookId);
+  const location = useLocation();
 
   return (
     <div>
       <Title />
       <div>
-        {findBook ? (
-          <div key={findBook.id}>
-            <BookCardDetail
-              id={findBook.id}
-              thumbnail={findBook.thumbnail}
-              title={findBook.title}
-              author={findBook.author}
-              description={findBook.description}
-            />
-          </div>
-        ) : null}
+        <div>
+          <BookCardDetail data={location.state.dataDetail} />
+        </div>
       </div>
     </div>
   );
