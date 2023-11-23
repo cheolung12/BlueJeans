dd2febd93ce71285f42dde5230313281144bb5a2
NavyBlueSky <lylebang9397@gmail.com>
2023-11-23 20:10:04 +0900

    [design] 일자리 상세 페이지 수정

--- client/src/components/Recruitment/Detail/DetailExample.jsx
+++ client/src/components/Recruitment/Detail/DetailExample.jsx
@@ -3,11 +3,33 @@ import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faWonSign, faMapLocationDot, faCalendarDays, faClock } from '@fortawesome/free-regular-svg-icons';
 import '@fortawesome/fontawesome-free/js/all.js';
+import ResButton from '../../common/ResButton';
+import Modal from 'react-modal';
 
 export default function DetailExample({ data }) {
     const dataD = data.state.dataDetail;
 
     const sampleimg = '/images/s1.jpeg';
+
+    const [isOpen, setIsOpen] = useState(false);
+    // Modal 스타일
+    const customStyles = {
+        overlay: {
+            backgroundColor: 'rgba(0,0,0,0.5)',
+        },
+        content: {
+            left: '0',
+            margin: 'auto',
+            width: '500px',
+            height: 'fit-content',
+            padding: '0',
+            overflow: 'hidden',
+        },
+    };
+
+    const modalToggle = () => {
+        setIsOpen(!isOpen);
+    };
 
     return (
         <>
@@ -18,14 +40,31 @@ export default function DetailExample({ data }) {
                         <img className="w-full h-auto " src={sampleimg} alt="직업 소개 이미지" />
                     </div>
                     <div className="m-2">글쓴이 프로필</div>
-                    <div className="m-2 text-2xl font-bold flex">
-                        <p className="">제목-{dataD.title}</p>
-                        <div className="flex justify-end">
-                            <p className="">마감 여부</p>
-                            <p className="">공고 지원 하기</p>
+                    <div className="m-2  flex justify-between">
+                        <div className="text-justify flex">
+                            <p className="text-2xl font-bold ">제목-{dataD.title}</p>
+                        </div>
+                        <div className="flex">
+                            {/* true => dataD.recruiting */}
+                            {true ? (
+                                <div>
+                                    <button className="bg-chatColor p-2 rounded-md hover:bg-opacity-80" onClick={modalToggle}>
+                                        공고 지원하기
+                                    </button>
+
+                                    <Modal isOpen={isOpen} ariaHideApp={false} style={customStyles}>
+                                        <div>{dataD.contact}</div>
+                                        <div className="flex justify-end pr-4">
+                                            <button onClick={modalToggle}>[ 닫기 ]</button>
+                                        </div>
+                                    </Modal>
+                                </div>
+                            ) : (
+                                <ResButton text={'마감'} />
+                            )}
                         </div>
                     </div>
-                    <div className="m-2 text-gray-600">공고게시일 ex.7일전 or 년월일</div>
+                    <div className="m-2 text-gray-600">공고게시일 ex.7일전 or 년월일 - dataD.createdAt가공</div>
                     <hr />
                     <div className="m-2 flex flex-col space-y-4">
                         <div className="text-xl font-semibold">정보</div>
