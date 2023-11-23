508eaac1d364522ab0a75d971d3ebb8b64b0435d
유민 <gkjm123@naver.com>
2023-11-24 00:27:43 +0900

    [design] e-book 로고 추가

--- client/src/components/Ebook/Main/Title.jsx
+++ client/src/components/Ebook/Main/Title.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { MdMenuBook } from 'react-icons/md';
 
 function Title() {
   return (
-    <div>
-      <div className='text-4xl text-center font-bold my-[3rem]'>E-book</div>
+    <div className='flex justify-center'>
+      <div className='flex items-center'>
+        <div>
+          <MdMenuBook className='text-4xl mr-2' />
+        </div>
+        <div className='text-4xl text-center font-bold my-[3rem]'>E-book</div>
+      </div>
     </div>
   );
 }
