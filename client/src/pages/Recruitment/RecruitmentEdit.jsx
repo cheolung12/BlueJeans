6432ec9dc11d527274fd103a320145a5dba70017
NavyBlueSky <lylebang9397@gmail.com>
2023-11-26 02:56:52 +0900

    [feat] 일자리 수정

--- client/src/pages/Recruitment/RecruitmentEdit.jsx
+++ client/src/pages/Recruitment/RecruitmentEdit.jsx
@@ -1,10 +1,24 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+import DaumPostcode from 'react-daum-postcode';
+import Modal from 'react-modal';
 
-export default function RecruitmentEdit() {
+export default function RecruitmentEdit({ data }) {
+    const [recruitmentEditData, setRecruitmentEditData] = useState({
+        title: data.title,
+        moneyStandard: data.moneyStandard,
+        money: data.money,
+        region: data.region,
+        content: data.content,
+        contact: data.contact,
+        workDay: data.workDay,
+        workTime: data.workTime,
+    });
+
     return (
         <>
-            <article></article>
+            <article>수정페이지</article>
         </>
     );
 }
