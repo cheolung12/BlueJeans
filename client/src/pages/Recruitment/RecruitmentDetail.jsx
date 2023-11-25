a41fc99d7fb49232ed90e623b3b6d3a7c8be2fee
NavyBlueSky <lylebang9397@gmail.com>
2023-11-26 04:14:59 +0900

    [feat] 일자리 마감

--- client/src/pages/Recruitment/RecruitmentDetail.jsx
+++ client/src/pages/Recruitment/RecruitmentDetail.jsx
@@ -1,13 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import DetailExample from '../../components/Recruitment/Detail/DetailExample';
 import { Link, useLocation } from 'react-router-dom';
 import ResButton from '../../components/common/ResButton';
+import axios from 'axios';
 
 export default function RecruitmentDetail({}) {
     const location = useLocation();
     console.log(location.state.dataDetail);
 
     // 권한 여부
+    // 게시물 등록자와 현재 로그인된 유저가 같을 시에 => boolean => 마감 수정 삭제 버튼 활성화
     const [editA, setEditA] = useState(true);
     // if ('권한있으면') {
     //     setEditA(true);
@@ -15,12 +17,57 @@ export default function RecruitmentDetail({}) {
     //     setEditA(false);
     // }
 
+    // 마감 하기
+    const [recruitingButton, setRecruitingButton] = useState('마감 하기');
+    const [recruitChange, setRecruitChange] = useState(true); //recruiting값
+
+    // 마감 버튼
+    function recruitClick() {
+        // if (location.state.dataDetail.recruitng) {
+        if (recruitingButton === '마감 하기' && recruitChange) {
+            setRecruitingButton('다시 모집');
+            setRecruitChange(false);
+        } else if (recruitingButton === '다시 모집' && !recruitChange) {
+            setRecruitingButton('마감 하기');
+            setRecruitChange(true);
+        }
+        console.log(recruitingButton, recruitChange);
+    }
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios({
+                    method: 'POST',
+                    url: `http://localhost:8080/api/recruiting/{job_id}`,
+                });
+                console.log(response);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+        fetchData();
+    }, [recruitChange]);
+
+    // 공고 수정
     function editRecruit() {
         console.log('공고 수정');
     }
-    function deleteRecruit() {
+
+    // 공고 삭제
+    const deleteRecruit = async () => {
         console.log('공고 삭제');
-    }
+        try {
+            const response = await axios({
+                method: 'DELETE',
+                url: `http://localhost:8080/api/location.state.dataDetail.id`,
+                // url: `http://localhost:8080/api/jobs/{job_id)`,
+            });
+            console.log(response);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
 
     return (
         <>
@@ -30,6 +77,7 @@ export default function RecruitmentDetail({}) {
                     <nav className="flex justify-end">
                         {editA ? (
                             <div className="flex flex-row  space-x-2">
+                                <button onClick={recruitClick}>{recruitingButton}</button>
                                 <button onClick={editRecruit}>수정</button>
                                 <button onClick={deleteRecruit}>삭제</button>
                                 <Link to={`/recruitment`}>
