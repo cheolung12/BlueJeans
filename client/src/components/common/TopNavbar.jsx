0d24b45785f559b3c472d50e12c3afa5962d0086
cheolung12 <109952479+cheolung12@users.noreply.github.com>
2023-11-27 05:17:31 +0900

    [feat] 로그아웃, 조건부렌더링

--- client/src/components/common/TopNavbar.jsx
+++ client/src/components/common/TopNavbar.jsx
@@ -1,36 +1,69 @@
 import React from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 
 export default function TopNavbar() {
+  const handleLogout = async () => {
+    const res = await axios({
+      method: 'GET',
+      url: 'http://localhost:8080/api/logout',
+      withCredentials: true,
+    });
+    if (res.data === 'redirect:/login') {
+      sessionStorage.clear();
+      window.location.reload();
+    } else {
+      console.log('로그아웃 실패!');
+    }
+  };
+
   return (
-    <nav class='bg-white fixed w-full top-0 z-99'>
-      <div class='flex flex-wrap justify-between items-center mx-auto max-w-screen-xl py-4 px-4'>
+    <nav className='bg-white fixed w-full top-0 z-99'>
+      <div className='flex flex-wrap justify-between items-center mx-auto max-w-screen-xl py-4 px-4'>
         <Link
           to='/'
-          class='flex items-center space-x-3 rtl:space-x-reverse'
+          className='flex items-center space-x-3 rtl:space-x-reverse'
         >
           <img
             src='https://flowbite.com/docs/images/logo.svg'
-            class='h-8'
+            className='h-8'
             alt='Flowbite Logo'
           />
-          <span class='self-center sm:text-4xl text-2xl  font-semibold whitespace-nowrap dark:text-white'>
+          <span className='self-center sm:text-4xl text-2xl  font-semibold whitespace-nowrap dark:text-white'>
             BlueJeans
           </span>
         </Link>
-        <div class='flex items-center space-x-6 rtl:space-x-reverse'>
-              <Link
-                to='/signup'
-                class='sm:text-xl text-md font-semibold  text-black hover:underline'
-              >
-                회원가입
-              </Link>
-              <Link
-                to='/login'
-                class='sm:text-xl text-md font-semibold  text-black hover:underline'
-              >
-                로그인
-              </Link>
+        <div className='flex items-center space-x-6 rtl:space-x-reverse'>
+          {!sessionStorage.getItem('isLogin') ? (
+            <Link
+              to='/signup'
+              className='sm:text-xl text-md font-semibold  text-black hover:underline'
+            >
+              회원가입
+            </Link>
+          ) : (
+            <Link
+              to='/mypage'
+              className='sm:text-xl text-md font-semibold  text-black hover:underline'
+            >
+              마이페이지
+            </Link>
+          )}
+          {!sessionStorage.getItem('isLogin') ? (
+            <Link
+              to='/login'
+              className='sm:text-xl text-md font-semibold  text-black hover:underline'
+            >
+              로그인
+            </Link>
+          ) : (
+            <div
+              onClick={handleLogout}
+              className='sm:text-xl text-md font-semibold  text-black hover:underline cursor-pointer'
+            >
+              로그아웃
+            </div>
+          )}
         </div>
       </div>
     </nav>
