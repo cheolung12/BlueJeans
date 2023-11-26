891967059da28dfd7b42aa0b7dba1f51ad36b10d
cheolung12 <109952479+cheolung12@users.noreply.github.com>
2023-11-27 03:41:43 +0900

    [design] top navbar

--- client/src/components/common/TopNavbar.jsx
+++ client/src/components/common/TopNavbar.jsx
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom';
 
 export default function TopNavbar() {
   return (
-    <nav class='bg-white border-gray-200 dark:bg-gray-900 mb-4'>
+    <nav class='bg-white fixed w-full top-0 z-99'>
       <div class='flex flex-wrap justify-between items-center mx-auto max-w-screen-xl py-4 px-4'>
-        <a
-          href='https://flowbite.com'
+        <Link
+          to='/'
           class='flex items-center space-x-3 rtl:space-x-reverse'
         >
           <img
@@ -14,20 +14,20 @@ export default function TopNavbar() {
             class='h-8'
             alt='Flowbite Logo'
           />
-          <span class='self-center sm:text-3xl text-xl  font-semibold whitespace-nowrap dark:text-white'>
+          <span class='self-center sm:text-4xl text-2xl  font-semibold whitespace-nowrap dark:text-white'>
             BlueJeans
           </span>
-        </a>
+        </Link>
         <div class='flex items-center space-x-6 rtl:space-x-reverse'>
               <Link
-                to='/login'
-                class='sm:text-lg text-sm font-semibold  text-black hover:underline'
+                to='/signup'
+                class='sm:text-xl text-md font-semibold  text-black hover:underline'
               >
                 회원가입
               </Link>
               <Link
-                to='/signup'
-                class='sm:text-lg text-sm font-semibold  text-black hover:underline'
+                to='/login'
+                class='sm:text-xl text-md font-semibold  text-black hover:underline'
               >
                 로그인
               </Link>
