d98781238194ecebe14659dc6d4315f6b01cae30
cheolung12 <109952479+cheolung12@users.noreply.github.com>
2023-11-26 17:01:38 +0900

    [fix] 로그인 요청 경로 보호

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import TopNavbar from './components/common/TopNavbar';
 
 function App() {
+  const location = useLocation();
+  const path = ['/login', '/signup'];
+
   return (
     <>
-      {/* <Navbar /> */}
-      <TopNavbar />
+     {!path.includes(location.pathname) && <TopNavbar />}
       <Outlet />
-      {/* <Footer /> */}
     </>
   );
 }
