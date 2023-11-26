ad4ddc550aa3cea017d2612f86d18bb688caffff
cheolung12 <109952479+cheolung12@users.noreply.github.com>
2023-11-27 03:40:38 +0900

    [feat] 레이아웃

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,25 @@ import './App.css';
 import { Outlet } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 import TopNavbar from './components/common/TopNavbar';
+import SideNavBar from './components/common/SideNavBar';
+import Wrapper from './components/common/Wrapper';
+import Footer from './components/common/Footer';
 
 function App() {
   const location = useLocation();
-  const path = ['/login', '/signup'];
-
+  const noSideNavbarPath = ['/', '/login', '/signup'];
+  const noTopNavbarPath = ['/login', '/signup'];
+  const noFooterPath = [];
   return (
     <>
-     {!path.includes(location.pathname) && <TopNavbar />}
-      <Outlet />
+      {!noTopNavbarPath.includes(location.pathname) && <TopNavbar />}
+      <Wrapper>
+        <div className='flex justify-between sm:flex-row flex-col items-center w-full px-12 mb-20'>
+          {!noSideNavbarPath.includes(location.pathname) && <SideNavBar />}
+          <Outlet />
+        </div>
+        <Footer />
+      </Wrapper>
     </>
   );
 }
