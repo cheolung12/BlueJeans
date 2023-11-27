adde6f16730930b4694d30117d52a715f61126bf
Asong Ma <136426795+asonghub@users.noreply.github.com>
2023-11-27 17:38:18 +0900

    [feat] 댓글 entity 만들기

---
+++ server/src/main/java/com/bluejeans/server/entity/EssayCommentsEntity.java
@@ -0,0 +1,43 @@
+package com.bluejeans.server.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.sql.Timestamp;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="essay_comments")
+public class EssayCommentsEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name="writer_id")
+    private UserEntity user;
+
+    @OneToOne(cascade = CascadeType.REMOVE)
+    @JoinColumn(name="essay_id")
+    private EssayEntity essay;
+
+    @Column
+    private String comment;
+
+    @CreationTimestamp
+    private Timestamp created_at;
+
+    @UpdateTimestamp
+    private Timestamp updated_at;
+
+
+}
