0e751aef5fb8353c09736eed6265ed76fa962617
LeeYoungKyung <128415146+LeeYoungKyung@users.noreply.github.com>
2023-11-28 02:17:02 +0900

    [feat] skeleton

---
+++ client/src/components/home/Skeleton.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { FaMountainSun } from 'react-icons/fa6';
+
+const Sktelecom = () => {
+  return (
+    <div className='animate-pulse flex items-center justify-center bg-gray-300 w-[53.125rem] h-[34.375rem] rounded'>
+      <FaMountainSun className='h-24 w-24 opacity-30' />
+    </div>
+  );
+};
+
+export default Sktelecom;
