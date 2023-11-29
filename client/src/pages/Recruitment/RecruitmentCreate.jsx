9d9d1eef872f73845e92cfbd95a85b5afb0caee0
NavyBlueSky <lylebang9397@gmail.com>
2023-11-30 01:03:32 +0900

    [feat] 공고 게시 유효성

--- client/src/pages/Recruitment/RecruitmentCreate.jsx
+++ client/src/pages/Recruitment/RecruitmentCreate.jsx
@@ -68,6 +68,18 @@ export default function RecruitmentCreate() {
         recruitData.append('content', recruitmentData.content);
         recruitData.append('file', file);
 
+        if (recruitData.get('region').length == 0) {
+            alert('지역을 입력하세요');
+            return;
+        }
+
+        const numericRegex = /^[0-9]+$/;
+
+        if (!numericRegex.test(recruitData.get('contact'))) {
+            alert('연락처에는 숫자만 입력하세요');
+            return;
+        }
+
         try {
             const response = await axios({
                 method: 'POST',
@@ -213,8 +225,8 @@ export default function RecruitmentCreate() {
                         name="title"
                         type="text"
                         placeholder="제목을 입력하세요."
-                        required
                         className="w-100 m-2 sm:h-12 h-9 p-2.5 sm:text-base text-xs block border rounded-lg text-gray-900 bg-gray-50 focus:outline-none focus:ring focus:ring-indigo-400"
+                        required
                     />
                 </div>
 
@@ -225,7 +237,7 @@ export default function RecruitmentCreate() {
                         {daysOfWeek.map((day, index) => (
                             <div
                                 key={index}
-                                className={`w-16 h-16 rounded-full flex items-center justify-center m-2 cursor-pointer ${
+                                className={`w-16 h-16 rounded-full flex items-center justify-center m-2 border cursor-pointer ${
                                     clickedDays.includes(day) ? 'bg-signatureColor text-white' : 'bg-gray-50 text-black'
                                 }`}
                                 onClick={() => handleClick(day)}
@@ -268,11 +280,11 @@ export default function RecruitmentCreate() {
                             </label>
                             <input
                                 id="money"
-                                className="w-100 m-2 sm:h-12 h-9 p-2.5 sm:text-base text-xs block border rounded-lg text-gray-900 bg-gray-50 focus:outline-none focus:ring focus:ring-indigo-400"
+                                className="w-100 m-2 sm:h-12 h-9 p-2.5 sm:text-base text-xs block border rounded-lg text-gray-900 bg-gray-50 focus:outline-none focus:ring focus:ring-indigo-400 remove-arrow"
                                 value={recruitmentData.money}
                                 onChange={handleInputChange}
                                 name="money"
-                                type="text"
+                                type="number"
                                 placeholder="급여를 입력하세요"
                                 required
                             />
@@ -370,7 +382,6 @@ export default function RecruitmentCreate() {
                         readOnly
                         placeholder="클릭하면 주소검색창이 나타납니다."
                         className="w-100 m-2 sm:h-12 h-9 p-2.5 sm:text-base text-xs block border rounded-lg text-gray-900 bg-gray-50 focus:outline-none focus:ring focus:ring-indigo-400"
-                        required
                     />
                     <Modal isOpen={isOpen} ariaHideApp={false} style={customStyles}>
                         <DaumPostcode onComplete={completeHandler} height="100%" />
@@ -469,6 +480,7 @@ export default function RecruitmentCreate() {
                         id="contact"
                         className="w-100 m-2 sm:h-12 h-9 p-2.5 sm:text-base text-xs block border rounded-lg text-gray-900 bg-gray-50 focus:outline-none focus:ring focus:ring-indigo-400"
                         value={recruitmentData.contact}
+                        maxlength="11"
                         onChange={handleInputChange}
                         name="contact"
                         type="text"
