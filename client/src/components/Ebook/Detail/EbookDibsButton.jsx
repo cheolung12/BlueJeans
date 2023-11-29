2b74fed03a21f2904c4737801dd5bdee1e6e96ce
유민 <gkjm123@naver.com>
2023-11-29 20:10:19 +0900

    [feat] 찜버튼

--- client/src/components/Ebook/Detail/EbookDibsButton.jsx
+++ client/src/components/Ebook/Detail/EbookDibsButton.jsx
@@ -1,26 +1,27 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { GoThumbsup } from 'react-icons/go';
+import { IoMdHeartEmpty } from 'react-icons/io';
+import { IoMdHeart } from 'react-icons/io';
 import { useParams } from 'react-router-dom';
 
-export default function AssayDibsButton({ like, notlike }) {
-  const { EssayId } = useParams();
+export default function EbookDibsButton({ like, notlike }) {
+  const { bookId } = useParams();
   // 하트 색상 변경
   const [isLikeAdd, setIsLikeAdd] = useState(false);
   // 찜하기 수 카운트
   const [likeCount, setLikeCount] = useState(0);
 
   //찜하기 버튼
-  const likeCountHandler = async () => {
+  const onClick = async () => {
     try {
       const response = await axios({
         method: 'POST',
-        url: `${process.env.REACT_APP_SERVER}/essays/detail/${EssayId}/likes`,
+        url: `${process.env.REACT_APP_SERVER}/ebook/like/${bookId}`,
         withCredentials: true,
       });
       console.log(response);
       setIsLikeAdd((prevIsAdd) => !prevIsAdd);
-      // setLikeCount(likeCount+1);
+      // setLikeCount(likeCount + 1);
     } catch (error) {
       console.error('Error fetching data:', error);
     }
@@ -29,12 +30,12 @@ export default function AssayDibsButton({ like, notlike }) {
   return (
     <div
       className='flex flex-col items-center cursor-pointer'
-      onClick={likeCountHandler}
+      onClick={onClick}
     >
       {!isLikeAdd ? (
-        <GoThumbsup className='text-4xl' />
+        <IoMdHeartEmpty className='text-4xl text-gray-700' />
       ) : (
-        <GoThumbsup className='text-4xl text-yellow-300' />
+        <IoMdHeart className='text-4xl text-red-600' />
       )}
 
       <span className='pt-1 text-sm'>
