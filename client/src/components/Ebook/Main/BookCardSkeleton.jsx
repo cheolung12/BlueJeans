04fd69e62766b5d245587336f17eb7bbc773245e
유민 <gkjm123@naver.com>
2023-12-04 01:09:29 +0900

    [desgin] ebook 스켈레톤

--- client/src/components/Ebook/Main/BookCardSkeleton.jsx
+++ client/src/components/Ebook/Main/BookCardSkeleton.jsx
@@ -7,7 +7,21 @@ export default function BookCardSkeleton() {
     <>
       <div>
         <div className='flex flex-col items-center justify-around w-[11rem] h-[21rem]'>
-          <div className=' w-[10rem] h-[13rem]'>
+          <div
+            className=' w-[10rem] h-[13rem]'
+            style={{
+              WebkitBoxShadow:
+                '0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23)',
+              MozBoxShadow:
+                '0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23)',
+              MsBoxShadow:
+                '0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23)',
+              OBoxShadow:
+                '0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23)',
+              boxShadow:
+                '0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23)',
+            }}
+          >
             <Skeleton className=' w-[10rem] h-[13rem]' />
           </div>
           <div className='flex flex-col text-center w-[10rem] h-[6rem]'>
