52d79f45f327cbacd52d5560836190bc42e0178a
cheolung12 <109952479+cheolung12@users.noreply.github.com>
2023-12-03 17:20:58 +0900

    [edit] change image

--- client/src/components/common/ImageSlider.jsx
+++ client/src/components/common/ImageSlider.jsx
@@ -1,31 +1,31 @@
 import React, { useState, useEffect } from 'react';
 
 export default function ImageSlider() {
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
-  const imageUrls = [
-    '/images/s1.jpeg',
-    '/images/s2.jpeg',
-    '/images/s3.jpeg',
-    '/images/s4.jpeg',
-  ];
-  const currentImageUrl = imageUrls[currentImageIndex];
+  // const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  // const imageUrls = [
+  //   '/images/s1.jpeg',
+  //   '/images/s2.jpeg',
+  //   '/images/s3.jpeg',
+  //   '/images/s4.jpeg',
+  // ];
+  // const currentImageUrl = imageUrls[currentImageIndex];
 
   // 3초마다 이미지 변경
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % imageUrls.length);
-    }, 3000);
-    return () => clearInterval(intervalId);
-  }, [currentImageIndex, imageUrls.length]);
+  // useEffect(() => {
+  //   const intervalId = setInterval(() => {
+  //     setCurrentImageIndex((prevIndex) => (prevIndex + 1) % imageUrls.length);
+  //   }, 3000);
+  //   return () => clearInterval(intervalId);
+  // }, [currentImageIndex, imageUrls.length]);
 
   return (
     <div
       className='w-[1200px] h-full lg:block hidden'
       style={{
-        backgroundImage: `url(${currentImageUrl})`,
+        backgroundImage: `url(${'/images/main-vt.png'})`,
         backgroundSize: 'cover',
         backgroundPosition: 'center',
-        transition: 'background-image 1s ease-in-out',
+        // transition: 'background-image 1s ease-in-out',
       }}
     ></div>
   );
