704826fb5068c921f389f52f8e72e242a8371c34
LeeYoungKyung <128415146+LeeYoungKyung@users.noreply.github.com>
2023-12-03 15:49:29 +0900

    [fix] find home

--- client/src/components/home/AddressButton.jsx
+++ client/src/components/home/AddressButton.jsx
@@ -2,36 +2,21 @@ import React from 'react';
 import { RiHomeHeartFill } from 'react-icons/ri';
 import axios from 'axios';
 
-export default function AddressButton({ setUserAddress }) {
-  const handleClick = async () => {
-    try {
-      const response = await axios({
-        method: 'POST',
-        url: `${process.env.REACT_APP_SERVER}/findhome`,
-        withCredentials: true,
-      });
-
-      setUserAddress(response.data);
-      console.log(response.data);
-      // navigate(`/essay/detail/${EssayId}`);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  return (
-    <>
-      <button onClick={handleClick} className=' w-30 text-[#2e375d]'>
-        <RiHomeHeartFill className=' w-36 h-20 drop-shadow-md' />
-        <p className=' text-lg text-black font-bold'>집찾기</p>
-      </button>
-    </>
-  );
-}
-
 // export default function AddressButton({ setUserAddress }) {
-//   const handleClick = () => {
-//     setUserAddress(' 서울특별시 마포구 염리동 숭문길 47');
+//   const handleClick = async () => {
+//     try {
+//       const response = await axios({
+//         method: 'POST',
+//         url: `${process.env.REACT_APP_SERVER}/findhome`,
+//         withCredentials: true,
+//       });
+
+//       setUserAddress(response.data);
+//       console.log(response.data);
+//       // navigate(`/essay/detail/${EssayId}`);
+//     } catch (error) {
+//       console.error(error);
+//     }
 //   };
 
 //   return (
@@ -43,3 +28,21 @@ export default function AddressButton({ setUserAddress }) {
 //     </>
 //   );
 // }
+
+export default function AddressButton({ setUserAddress }) {
+  const handleClick = () => {
+    // setUserAddress(' 서울특별시 마포구 염리동 숭문길 47');
+    setUserAddress('서울특별시 동작구 상도로41가길 17');
+  };
+
+  return (
+    <>
+      <div className='flex justify-center'>
+        <button onClick={handleClick} className='w-30  text-[#2e375d]'>
+          <RiHomeHeartFill className=' w-36 h-20 drop-shadow-md' />
+          <p className=' text-lg text-black font-bold'>집찾기</p>
+        </button>
+      </div>
+    </>
+  );
+}
