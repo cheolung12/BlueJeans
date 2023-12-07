bf5a619b5efbb6f8e979f54f7d39307189a7e7e0
cheolung12 <109952479+cheolung12@users.noreply.github.com>
2023-12-08 03:47:27 +0900

    [feat] breakpoint 추가

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -8,6 +8,9 @@ module.exports = {
                 chatColor: '#4d70e3',
                 signatureColor: '#2C86ED',
             },
+            screens: {
+                '3xl': '1700px', 
+            },
         },
     },
     plugins: [require('@tailwindcss/line-clamp')],
